305203fc8f25e7b93eea68fbe527ddc441387219
Toby Woollums <woollumst@gmail.com>
2025-02-16 12:22:46 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    document.title = count !== 0 && `Counter App: ${count}` || "Counter App";
+  }, [count])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app-container" style={{textAlign: "center", marginTop: "50px"}}>
+        <h1>Counter App</h1>
+        <h2 className="counter">{count}</h2>
+        <button className="increase" onClick={() => setCount(count + 1)}>Increase</button>
+        <button className="decrease" onClick={() => setCount(count - 1)}>Decrease</button>
+        <button className="reset" onClick={() => setCount(0)}>Reset</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
