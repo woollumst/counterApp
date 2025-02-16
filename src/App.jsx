d8e3cfce1c2d25e5ebfb5089259b74cb34e0d057
Toby <woollumst@gmail.com>
2025-02-16 12:32:14 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,11 @@ function App() {
       <div className="app-container" style={{textAlign: "center", marginTop: "50px"}}>
         <h1>Counter App</h1>
         <h2 className="counter">{count}</h2>
+        <div className="button-container">
         <button className="increase" onClick={() => setCount(count + 1)}>Increase</button>
         <button className="decrease" onClick={() => setCount(count - 1)}>Decrease</button>
         <button className="reset" onClick={() => setCount(0)}>Reset</button>
+        </div>
       </div>
     </>
   )
